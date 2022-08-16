532a2dc51c1d7b7b78dad1e52f57ff49de3f42f7
M Wildan F <wildanfd11@gmail.com>
2022-08-16 19:28:05 +0700

    Menambahkan contoh dari tipe data string pada php

---
+++ php-basic/StringDataType.php
@@ -0,0 +1,18 @@
+<?php
+/** Escape Sequences :
+ * \" : Double Quote
+ * \\ : Backslash
+ * \$ : Membuat dollar sign 
+ * \n : enter atau membuat baris baru
+ * \t : tab
+ */
+// Single Quote - Tidak dapat menambahkan atau menggunakan escape sequence
+echo 'Nama : ';
+echo 'Jang Kyujin';
+echo "\n";
+
+//Double Quote - Dapat menambahkan escape sequence seperti \n, \t
+echo 'Nama : ';
+echo 'Kang Seulgi';
+echo "\n";
+
