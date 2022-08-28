4216b2be509d7bfc95207f34ec5da075121f8f84
M Wildan F <wildanfd11@gmail.com>
2022-08-28 22:17:00 +0700

    Menambahkan penerapan tipe data string

--- php-basic/StringDataType.php
+++ php-basic/StringDataType.php
@@ -6,13 +6,28 @@
  * \n : enter atau membuat baris baru
  * \t : tab
  */
-// Single Quote - Tidak dapat menambahkan atau menggunakan escape sequence
+// * 1. Single Quote - Tidak dapat menambahkan atau menggunakan escape sequence
 echo 'Nama : ';
 echo 'Jang Kyujin';
 echo "\n";
 
-//Double Quote - Dapat menambahkan escape sequence seperti \n, \t
+// * 2. Double Quote - Dapat menambahkan escape sequence seperti \n, \t
 echo 'Nama : ';
 echo 'Kang Seulgi';
 echo "\n";
 
+
+// * 3. Membuat string multiline menggunakan heredoc
+echo <<<TEST
+Ini adalah string 
+yang ditulis dengan format 
+multiline menggunakan heredoc
+
+TEST;
+
+// * 4. Membuat string multiline menggunakan nowdoc
+echo <<<'TEST'
+Ini adalah string 
+yang ditulis dengan format 
+multiline menggunakan nowdoc
+TEST;
