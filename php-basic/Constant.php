c732f93c619910ce054c0b9a9a09c3c0aa594768
M Wildan F <wildanfd11@gmail.com>
2022-09-04 12:10:48 +0700

    Constant pada PHP

---
+++ php-basic/Constant.php
@@ -0,0 +1,14 @@
+<?php
+// Membuat constant pada PHP
+    // Best practice constant => UPPER_CASE
+    // define(namaVariable, nilai)
+define("AUTHOR", "Jamal");
+define("APP_VERSION", 5);
+
+echo "Author : ";
+echo AUTHOR;
+echo "\n";
+
+echo "App Version : ";
+echo APP_VERSION;
+echo "\n";
